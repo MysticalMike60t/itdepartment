02111a3666950a09a86257d535284a81b3a88ccd
MysticalMike60t <caden.finkelstein@cttech.org>
2023-03-01 12:15:35 -0500

    Major crash fix

--- client/src/components/contact/ContactInfo.jsx
+++ client/src/components/contact/ContactInfo.jsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import $ from "jquery"
+
 import data from "../../data/contact/data.json";
 
 const Contacts = () => {
@@ -9,11 +12,30 @@ const Contacts = () => {
   let eTextSub;
   let aText = "Address:";
   let aTextSub;
+  const phone_e = document.getElementById("phone");
+  const email_e = document.getElementById("email");
+  const address_e = document.getElementById("address");
+  const show = (item) => {
+    alert(item);
+  }
   if (window.innerWidth <= 500) {
     pTextSub = pText.substring(0,pText.length-1);
     eTextSub = eText.substring(0,eText.length-1);
     aTextSub = aText.substring(0,aText.length-1);
   }
+  $(document).ready(function(){
+    if (window.innerWidth <= 500) {
+      $("#phone").unbind().click(function() {
+        show(data.phone);
+      });
+      $("#email").unbind().click(function() {
+        window.location.href = "mailto:" + email;
+      });
+      $("#address").unbind().click(function() {
+        show(data.address);
+      });
+}
+  })
   return (
     <div className="info-wrapper">
       <h4 id="phone">{pTextSub} <span>{data.phone}</span></h4>
