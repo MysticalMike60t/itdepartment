365647b1ac632ac686e0b5a62d0ba12d8f366ed4
MysticalMike60t <caden.finkelstein@cttech.org>
2023-05-23 09:32:14 -0400

    Fixed all issues

--- client/src/data/about/uniforms/data.js
+++ client/src/data/about/uniforms/data.js
@@ -6,6 +6,7 @@ const uniforms = [
     {id: "2", title: "Information Tech Crew Neck Sweatshirt", price: "21.50", link: "https://www.stillmanuniformsri.com/product/crew-neck-sweatshirt-3/"},
     {id: "3", title: "Information Tech Long Sleeve Polo Shirt", price: "24.00", link: "https://www.stillmanuniformsri.com/product/long-sleeve-polo-shirt-2/"},
     {id: "4", title: "Information Tech Pants Waist By Inseam", price: "26.00", link: "https://www.stillmanuniformsri.com/product/crew-neck-sweatshirt-copy/"},
-    {id: "5", title: "Information Tech Short Sleeve Polo Shirt", price: "21.50", link: "https://www.stillmanuniformsri.com/product/short-sleeve-polo-shirt-4/"}
+    {id: "5", title: "Information Tech Short Sleeve Polo Shirt", price: "21.50", link: "https://www.stillmanuniformsri.com/product/short-sleeve-polo-shirt-4/"},
+    {id: "0", title: "", price: "", link: ""}
 ]
 export { layout, uniforms };
