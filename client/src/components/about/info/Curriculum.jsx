2874bd1a991d345d5faa745d165d1bdb67588602
MysticalMike60t <caden.finkelstein@cttech.org>
2023-06-09 08:34:48 -0400

    Added grades

--- client/src/components/about/info/Curriculum.jsx
+++ client/src/components/about/info/Curriculum.jsx
@@ -10,21 +10,21 @@ const Curriculum = () => {
         </h1>
       </div>
       <div className="display display-credits">
-        {items.map((data,key)=>{
-          return(
-            <span className="item" key={key}><span>{data.credits}</span> <p className="text">Credits</p> </span>
+        {items.map((data, key) => {
+          return (
+            <span className="item item-credit" key={key}><span>{data.credits}</span><p className="text">Credits</p><p className='year'><span className='year__y'>y</span>{data.year}</p></span>
           )
         })}
       </div>
       <div className="display display-subject">
-        {items.map((data,key)=>{
-          return(
+        {items.map((data, key) => {
+          return (
             <span className="item item-subject" key={key}><span className="grade">{head[key].grade}</span><span className='subject'>{data.subject}</span></span>
           )
         })}
       </div>
-      {cards.map((data,key)=>{
-        return(
+      {cards.map((data, key) => {
+        return (
           <div className="card" key={key}>
             <h1 key={key}>{data.title} <span>-</span> Grade {data.grade}</h1>
             <p key={key}>{data.text}</p>
@@ -33,7 +33,7 @@ const Curriculum = () => {
       })}
       <div className="card last">
         <p>Students successfully completing this course of study will be able to pursue a two-year computer technology degree at state colleges and universities or go on to a four-year computer engineering degree. Students can obtain immediate employment in the Information Technology area. Jobs include: help desk assistant, sales, systems technician and database maintenance, to name a few. Students graduating from Information Technology also find great success in the military in fields like communications, information management and information technician.</p>
-        <br/>
+        <br />
       </div>
     </div>
   )
